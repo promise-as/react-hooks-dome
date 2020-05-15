e3e17f7bc62b8008fac148e075bb00cf10600a7f
Hong Baojin <13809735212.163.com>
2020-05-15 18:29:10 +0800

    hooks useContext

--- demo01/src/Example.js
+++ demo01/src/Example.js
@@ -1,4 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+function Index() {
+  useEffect(() => { console.log(useEffect => '老弟，你来了！Index页面') })
+  return <h2>hbj.com</h2>
+}
+
+function List() {
+  useEffect(() => { console.log(useEffect => '老弟，你来了！List页面') })
+  return <h2>List Page</h2>
+}
 
 function Example() {
   // 等同与下面一行 es5
@@ -11,14 +22,23 @@ function Example() {
 
   useEffect(() => {
     console.log(`useEffect => You clicked ${count} times`);
-  })
+    return () => {
+      console.log('========');
+    }
+  }, [count])
 
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <Router>
+        <li><Link to="/">首页</Link></li>
+        <li><Link to="/list/">列表</Link></li>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/list/" exact component={List} />
+      </Router>
+
     </div>
   );
 }
