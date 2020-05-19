e28e98109f2cf9dff4d8fc6ec9d850394b47f45d
Hong Baojin <13809735212.163.com>
2020-05-19 18:33:20 +0800

    study to React hooks useReder

--- commit-demo/src/App.js
+++ commit-demo/src/App.js
@@ -1,87 +1,37 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  // 声明一个新的叫做 “count” 的 state 变量
-  // let [list, setList] = useState(null);
-  let prevList = [
-    { username: '张三', content: 'React不错!' },
-    { username: '李四', content: 'React有点难!' }
-  ]
+  const [counterOne, setCounterOne] = useState(0)
+  const [counterTwo, setCounterTwo] = useState(0)
 
-  let [list, setList ] = useState(prevList);
+  const incrementOne = () => {
+    setCounterOne(counterOne + 1)
+  }
 
-  const inputUser = useRef(null);
-  const inputContent = useRef(null);
+  const incrementTwo = () => {
+    setCounterTwo(counterTwo + 1)
+  }
 
-  useEffect(() => {
-
-    setList = () => {
-      let username = inputUser.current.value
-      let content = inputContent.current.value
-
-      list.unshift({username, content})
-
-      prevList = list
-
-      console.log(list, prevList);
-    }
-
-    return () => {
-      // console.log(list, prevList);
-    }
-     
-  }, [prevList])
+  const isEven = () => {
+    let i = 0
+    // while (i < 10000000) i += 1
+    return counterOne % 2 === 0
+  }
 
   return (
     <div>
-      <header className="site-header jumbotron">
-        <div className="container">
-          <div className="row">
-            <div className="col-xs-12">
-              <h1>请发表对React的评论</h1>
-            </div>
-          </div>
-        </div>
-      </header>
-
-      <div className="container">
-        <div className="col-md-4">
-          <form className="form-horizontal">
-            <div className="form-group">
-              <label>用户名</label>
-              <input ref={inputUser} type="text" className="form-control" placeholder="用户名"/>
-            </div>
-            <div className="form-group">
-              <label>评论内容</label>
-              <textarea ref={inputContent} className="form-control" rows="6" placeholder="评论内容"></textarea>
-            </div>
-            <div className="form-group">
-              <div className="col-sm-offset-2 col-sm-10">
-                <button type="button" className="btn btn-primary pull-right" onClick={() => setList()}>提交</button>
-              </div>
-            </div>
-          </form>
-        </div>
-        <div className="col-md-8">
-          <h3 className="reply">评论回复：</h3>
-          <h2 style={{ 'display': 'none' }}>暂无评论，点击左侧添加评论！！！</h2>
-          <ul className="list-group">
-            {
-              list.map((item, index) => {
-                return (
-                  <li className="list-group-item" key={index}>
-                    <div className="handle">
-                      <span className="btn btn-danger">删除</span>
-                    </div>
-                    <p className="user"><span >{item.username}</span><span>说:</span></p>
-                    <p className="centence">{item.content}</p>
-                  </li>
-                )
-              })
-            }
-          </ul>
-        </div>
-      </div>
+      <button
+        onClick={incrementOne}
+      >Count One = {counterOne}</button>
+      <span>
+        {
+          isEven() ? 'even' : 'odd'
+        }
+      </span>
+        <br/>
+        <button
+          onClick={incrementTwo}
+        >Count Two = {counterTwo}</button>
     </div>
   )
 }
--- demo01/src/index.js
+++ demo01/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Example from './Example';
+import Example from './example6/Example6';
 
 ReactDOM.render(<Example />, document.getElementById('root'))
